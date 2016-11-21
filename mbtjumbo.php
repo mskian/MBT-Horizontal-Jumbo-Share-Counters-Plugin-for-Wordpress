846b532af0548c8e9c8c7612ae940c6938969dad
Santhosh veer <mskian@users.noreply.github.com>
2016-11-21 22:21:09 +0530

    Update mbtjumbo.php

--- mbtjumbo.php
+++ mbtjumbo.php
@@ -106,6 +106,18 @@ if (is_single()) {
 <span id="mbtcount" class="count anim"></span></span> 
 </div> 
 </div>
+<!--BUFFER--> 
+<div id="MBTshares"> 
+<div class="mbt-o"> 
+<a class="mbt-gp" href="https://buffer.com/add?url='.get_permalink( $post->ID).'&picture='.wp_get_attachment_url( get_post_thumbnail_id($post->ID) ).'&text='.get_the_title( $post->ID).'&via=allwebtuts" onclick="window.open(this.href, \'sharer\', \'toolbar=0, scrollbars,status=0,  width=626,height=436\');  return false;" rel="nofollow" target="_blank" title="Share on Buffer!"><img class="bufferimg" src=" https://3.bp.blogspot.com/-eYyUk4KGuBg/Vs-oFm656VI/AAAAAAAAQfE/maUNyJ9PWqE/s1600/logo-icon.png"/><span class="label">Buffer</span></a> 
+</div> 
+<div class="mbtcount-o ext"> 
+<div class="arrow"><s></s><i></i></div> 
+<span class="share-btn" data-service="buffer"> 
+<span class="count anim" id="mbtcount"></span></span> 
+</div> 
+</div>
+
 <!-- WHATSAPP --> 
   <div id="MBTshares"> 
     <div class="mbt-o"> 
